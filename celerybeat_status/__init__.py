e512a79f11f866ac150f0db69894624acb0e1813
Hugo Bessa <hugo@bessa.me>
2017-12-12 17:00:22 -0300

    fixes version

--- celerybeat_status/__init__.py
+++ celerybeat_status/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'CeleryBeat Status'
-__version__ = '0.0.7'
+__version__ = '0.0.9'
 __author__ = 'Vinta Software'
 __license__ = 'MIT License'
 __copyright__ = 'Copyright 2017 Vinta Serviços e Soluções Tecnológicas Ltda'
