3e4b229b11b1d504141effcaf1097e9eda0ec8f5
Hugo Bessa <hugo@bessa.me>
2018-02-05 14:43:21 -0300

    passes class and self to super to support python2

--- celerybeat_status/views.py
+++ celerybeat_status/views.py
@@ -17,7 +17,7 @@ class PeriodicTasksStatusListView(TemplateView):
         return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = super(PeriodicTasksStatusListView, self).get_context_data(**kwargs)
         context['title'] = _('Periodic tasks status')
         context['user'] = self.request.user
         context['site_url'] = self.site_url
